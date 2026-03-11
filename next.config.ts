3af0de921a6eb11a7ac1424d066c3796c916aa6a
Praneeth Geethanjana <praneethg@ceyentra.com>
2026-03-11 22:43:37 +0530

    updated config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/luxury-villa",
-  assetPrefix: "/luxury-villa/",
   images: {
     remotePatterns: [
       {
